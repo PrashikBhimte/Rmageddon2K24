c0dde11847057a669ef06317fc3bc60673e6d40f
PrashikBhimte <2022bcs007@sggs.ac.in>
2023-12-06 00:40:36 +0530

    initial background is added

---
+++ clientside/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Background from './Components/Background.jsx';
+
+function App() {
+  return (
+    <div className="App">
+    <Background />
+    </div>
+  );
+}
+
+export default App;
