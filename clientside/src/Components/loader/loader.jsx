1fdf263a30a673cd8b3fc1ff90d873cf436b3b74
PrashikBhimte <2022bcs007@sggs.ac.in>
2023-12-26 22:48:51 +0530

    loader is updated

--- clientside/src/Components/loader/loader.jsx
+++ clientside/src/Components/loader/loader.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import "./loader.css";
-import logo from "../../images/logo.png";
+// import logo from "../../images/logo.png";
+import video from "../../videos/loader.mp4";
 
 export default function Loader() {
   return (
     <div className='loader'>
-        <div className='loader-circle' >
+      <video autoPlay muted typeof='mp4/video'>
+        <source src={ video }></source>
+      </video>
+        {/* <div className='loader-circle' >
             <img src={logo} alt='logo' />
-        </div>
+        </div> */}
     </div>
   )
 }
