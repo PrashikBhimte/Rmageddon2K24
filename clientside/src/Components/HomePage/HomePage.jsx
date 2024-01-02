5b434d048f634db1d4383d44b4ab9aed26c5fd26
PrashikBhimte <2022bcs007@sggs.ac.in>
2024-01-02 19:04:19 +0530

    side scroll bar is added

--- clientside/src/Components/HomePage/HomePage.jsx
+++ clientside/src/Components/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import HeroPage from '../heropage/HeroPage';
 // import Spons from '../spons/Spons';
 import Footer from '../footer/footer';
@@ -8,8 +8,27 @@ import Card from '../gamesection/Card';
 import Unlock from '../unstop/Unlock';
 
 export default function HomePage() {
+
+  const [ scroll, setScroll ] = useState(0);
+
+  useEffect(() => {
+    const progressScrollHandler = () => {
+      const totalScroll = document.documentElement.scrollTop;
+      const windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      const scroll = totalScroll / windowHeight * 100;
+      setScroll(scroll);
+    }
+
+    window.addEventListener("scroll", progressScrollHandler);
+
+    return () => window.removeEventListener("scroll", progressScrollHandler);
+  });
+
   return (
-    <div>
+    <div className='homepage'>
+      <div className='vertical-bar'>
+        <div className='vertical-inner-bar' style={{ height : scroll + "%" }}></div>
+      </div>
       <div className='gradient' id='gradient1'></div>
       <div className='gradient' id='gradient2'></div>
       <div className='gradient' id='gradient3'></div>
